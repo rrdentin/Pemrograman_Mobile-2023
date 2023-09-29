2b721eda497a1dc50daf487ecde79148c21ab2d7
rrdentin <denti.dona@gmail.com>
2023-09-29 14:36:29 +0700

    minggu 5

---
+++ Pertemuan 5/quiz.dart
@@ -0,0 +1,74 @@
+void main(){
+  //Tugas 1
+  print('Hello world');
+
+  // //Tugas 2
+  String name = 'Rr. Denti Nurramadhona';
+
+  print(name);
+  print(name);
+  print(name);
+
+  //Tugas 3
+  String firstName = 'Rr. Denti';
+  final lastName = 'Nurramadhona';
+
+  print(firstName);
+  print(lastName);
+
+  //Tugas 4
+  final array1 = [1, 2, 3];
+  final array2 = [4, 5, 6];
+
+  array1[0] = 100;
+  array2[0] = 100;
+
+  print(array1);
+  print(array2);
+
+  // //Tugas 4
+  int number1 = 100;
+  double number2 = 100.5;
+
+  print(number1);
+  print(number2);
+
+  //Tugas 5
+  num number = 10;
+  print(number);
+
+  number = 10.5;
+  print(number);
+
+  //Tugas 6
+  String firstName = 'Rr. Denti';
+  String lastName = 'Nurramadhona';
+
+  var fulName = '$firstName ${lastName}';
+
+  print(fulName);
+
+  //Tugas 7
+  var text = 'ini \'OPTIMAL\' \$sekali';
+  print(text);
+
+  //Tugas 8
+  var name1 = firstName + lastName;
+  var name2 = 'Rr.' 'Denti' 'Nurramadhona';
+  print(name1);
+  print(name2);
+
+  //Tugas 9
+  var name1 = firstName + " " + lastName;
+  var name2 = 'Rr.' ' Denti' ' Nurramadhona';
+  print(name1);
+  print(name2);
+
+  //Tugas 10
+  var kalmiat_panjang = '''
+  ini
+  saya punya
+  proyek dart
+  banyak ''';
+  print(kalmiat_panjang);
+}
